5d949e385972670cd14f8a1b300a5a0ff8d22b23
Yinhu Li <56863457+liyinhu@users.noreply.github.com>
2026-03-02 18:33:38 +0800

    Create 06.Pangenome_construction.sh

---
+++ 06.Pangenome_construction.sh
@@ -0,0 +1,3 @@
+mv /project/TM/01.funannotate/Strain*/predict_results/Talaromyces_marneffei.proteins.fa /project/TM/04.pangenome/Proteins/Strain*.faa
+
+orthofinder -f /project/TM/04.pangenome/Proteins/ -t 64
